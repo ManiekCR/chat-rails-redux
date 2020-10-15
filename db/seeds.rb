540f307e07cccef57873ae83f9c870ed74811099
Marian Caron <caron.marian@gmail.com>
2020-10-15 21:54:51 +0200

    create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+FIRSTNAMES = %w(Anna Bertha Elisabeth Emma Frieda Hans Heinz Helmut Herbert Karl Kurt)
+LASTNAMES = %w(Abel Aller Bach Bank Bayer Beck Bieber Bose Cruse Federer Fink Gabel Gossmann)
+
+puts 'wiping the database'
+  User.destroy_all
+  Message.destroy_all
+  Channel.destroy_all
+
+puts 'creating 5 channels'
+
+Channel.create!(name: 'Berlin')
+Channel.create!(name: 'Ruby on Rails')
+Channel.create!(name: 'React/Redux')
+Channel.create!(name: 'Javascript')
+Channel.create!(name: 'Fun')
+
+puts 'creating 5 fake users'
+
+5.times do
+  User.create!(
+    email: "#{FIRSTNAMES.sample}.#{LASTNAMES.sample}@example.com",
+    password: "123456",
+    name: FIRSTNAMES.sample
+  )
+end
+
