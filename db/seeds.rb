51890ce49a1648e8535cb0208b0b86c2aac2217b
Marian Caron <caron.marian@gmail.com>
2020-10-15 22:55:35 +0200

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,24 @@
-FIRSTNAMES = %w(Anna Bertha Elisabeth Emma Frieda Hans Heinz Helmut Herbert Karl Kurt)
-LASTNAMES = %w(Abel Aller Bach Bank Bayer Beck Bieber Bose Cruse Federer Fink Gabel Gossmann)
-
 puts 'wiping the database'
-  User.destroy_all
   Message.destroy_all
+  User.destroy_all
   Channel.destroy_all
 
-puts 'creating 5 channels'
+names = %w(general berlin react ruby)
+usernames = %w(Ghost Maniek George Abdel Klaus)
 
-Channel.create!(name: 'Berlin')
-Channel.create!(name: 'Ruby on Rails')
-Channel.create!(name: 'React/Redux')
-Channel.create!(name: 'Javascript')
-Channel.create!(name: 'Fun')
-
-puts 'creating 5 fake users'
-
-5.times do
-  User.create!(
-    email: "#{FIRSTNAMES.sample}.#{LASTNAMES.sample}@example.com",
-    password: "123456",
-    name: FIRSTNAMES.sample
-  )
+channels = names.map do |name|
+  Channel.find_or_create_by(name: name)
 end
 
+users = usernames.map do |username|
+  User.create(email: "#{username.downcase}@lewagon.com", username: username, password: "testtest")
+end
+
+20.times do
+  Message.create! user: users.sample, channel: channels.sample, content: Faker::TvShows::HowIMetYourMother.quote
+end
+
+puts 'Channels:'
+channels.each do |channel|
+  puts "- #{channel.id}: #{channel.name}"
+end
